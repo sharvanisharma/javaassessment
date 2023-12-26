2001fd74c4f2ed5a9708a73d3f9b67f524f9008a
sharvanisharma <153992469+sharvanisharma@users.noreply.github.com>
2023-12-26 17:06:01 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class string {
+    public static void main(String[] args) {
+        String s = "Sharvani";
+        System.out.println(s.length());
+        System.out.println(s.indexOf('i'));
+        System.out.println(s.contains("kani"));
+        System.out.println(s.endsWith("i"));
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+    }
+    
+}
