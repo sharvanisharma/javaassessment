04e0f28eb1026cdcc25e22b773192c4198335b3e
sharvanisharma <153992469+sharvanisharma@users.noreply.github.com>
2023-12-26 15:39:32 +0530

    Add files via upload

---
+++ arraylistcollections.java
@@ -0,0 +1,14 @@
+ import java.util.ArrayList;
+    public class arraylistcollections {
+
+    public static void main(String[] args){
+        ArrayList<String> animals = new ArrayList<>();
+        //adding elements
+        animals.add("tiger");
+        animals.add("lion");
+        animals.add("deer");
+
+        System.out.println("ArrayList: " + animals);
+    }
+}
+
