8672cece1746083c3e2fc28587aaadacc7300c5b
sharvanisharma <asharvani3108@gmail.com>
2023-12-26 11:09:06 +0530

    accessmodifiers.java

---
+++ accessmodifiers.java
@@ -0,0 +1,9 @@
+public class accessmodifiers {
+        public static void main(String[] args) {
+            System.out.println("Java program created in git repository");
+        }
+        
+    }
+    
+    
+
