10b72b46488735c5bd981c1a5c81799e24fc1089
sharvanisharma <153992469+sharvanisharma@users.noreply.github.com>
2024-01-18 19:46:26 +0530

    Delete accessmodifiers.java

--- accessmodifiers.java
+++
@@ -1,9 +0,0 @@
-public class accessmodifiers {
-        public static void main(String[] args) {
-            System.out.println("Java program created in git repository");
-        }
-        
-    }
-    
-    
-
